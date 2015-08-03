d1574e5eb7679fca0958815a249305619f9052f7
nico <nicolas.p.suarez@gmail.com>
2015-08-03 12:58:14 -0300

    added Massbuy model

---
+++ models/Massbuy.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "massbuy".
+ *
+ * @property integer $id
+ * @property integer $product_id
+ * @property integer $quantity
+ * @property string $utility_drop
+ *
+ * @property Product $product
+ */
+class Massbuy extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'massbuy';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+			[['quantity', 'utility_drop'], 'required'],
+            [['product_id', 'quantity'], 'integer'],
+            [['utility_drop'], 'number', 'min' => -100, 'max' => 100, 'numberPattern' => Currency::VALIDATOR_PATTERN]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'product_id' => 'Producto',
+            'quantity' => 'Cantidad',
+            'utility_drop' => 'Descuento de utilidad',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProduct()
+    {
+        return $this->hasOne(Product::className(), ['id' => 'product_id']);
+    }
+}
