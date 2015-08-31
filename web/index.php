7911d7ae5fa6aefb1bfdef79bfa6957c4cef6f0e
nico <nicolas.p.suarez@gmail.com>
2015-08-31 02:52:32 -0300

    modified index.php to use server APP_ENV

--- web/index.php
+++ web/index.php
@@ -1,8 +1,12 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+// Set config based on the environment
+// Use production as default
+$env = getenv('APP_ENV') ? getenv('APP_ENV') : 'prod';
+defined('YII_ENV') or define('YII_ENV', $env);
+if ($env == 'dev') {
+	defined('YII_DEBUG') or define('YII_DEBUG', true);
+}
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
