56e9b1d22c88bc514569d21d844e4f289cd1fc04
nico <nicolas.p.suarez@gmail.com>
2016-03-21 11:14:55 -0300

    fixed product description width on estimate pdf

--- views/estimate/pdf.php
+++ views/estimate/pdf.php
@@ -20,16 +20,16 @@ $i = 0;
 ?>
 
 <div style="background: #232323; color: lightgray;">
-	<div style="float:left; margin:0; width: 30%; margin-left: 35px">
+	<div style="float:left; margin: 0; width: 30%; margin-left: 35px">
 		<b>Imágen</b>
 	</div>
-	<div style="float:left; margin:0; width: 40%;">
+	<div style="float:left; margin: 0; width: 40%;">
 		<b>Código/Descripción</b>
 	</div>
-	<div style="float:left; margin:0; width: 10%; text-align: center;">
+	<div style="float:left; margin: 0; width: 10%; text-align: center;">
 		<b>Cantidad</b>
 	</div>
-	<div style="float:left; margin:0; width: 15%; text-align: center;">
+	<div style="float:left; margin: 0; width: 15%; text-align: center;">
 		<b>Precio unit.</b>
 	</div>
 </div>
@@ -40,24 +40,26 @@ $i = 0;
 	?>
 
 	<div style="margin-top: 20px;">
-		<div style="float:left; margin:0; width: 35%; text-align: center; height: 1px;">
+		<div style="float:left; margin: 0; width: 35%; text-align: center; height: 1px;">
 			<?= $product->imageUrl ? Html::img(ltrim($product->imageUrl, '/'), ['height' => 100]) : '' ?>
 		</div>
-		<div style="float:left; margin:0; width: 60%;">
-			<b><?= $product->bukmark_code ?></b>
-			<br>
-			<?= $product->title ?>
-			<br>
-			<?= $product->description ?>
-			<div style="float:left; margin:0;">
+		<div style="float: left; margin: 0; width: 60%;">
+			<div style="margin: 0; width: 58%;">
+				<b><?= $product->bukmark_code ?></b>
+				<br>
+				<?= $product->title ?>
+				<br>
+				<?= $product->description ?>
+			</div>
+			<div style="float:left; margin: 0;">
 				<?php foreach ($group as $entry): ?>
-					<div style="float:left; margin:0; width: 58%; height: 1px;">
+					<div style="float:left; margin: 0; width: 58%; height: 1px;">
 						<?= $entry->description ?>
 					</div>
-					<div style="float:left; margin:0; width: 30%; text-align: center; height: 1px;">
+					<div style="float:left; margin: 0; width: 30%; text-align: center; height: 1px;">
 						<?= $entry->quantity ?>
 					</div>
-					<div style="float:left; margin:0; text-align: center; height: 1px;">
+					<div style="float:left; margin: 0; text-align: center; height: 1px;">
 						<?= Currency::format($entry->subtotal, Currency::CURRENCY_ARS); ?>
 					</div>
 				<?php endforeach; ?>
