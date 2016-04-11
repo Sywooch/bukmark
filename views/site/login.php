9b8f858c4dc543ef6ef6a212361ebc5474ac3694
nico <nicolas.p.suarez@gmail.com>
2016-04-11 06:39:31 -0300

    fixed remember me checkbox

--- views/site/login.php
+++ views/site/login.php
@@ -29,9 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
 	<?= $form->field($model, 'password')->passwordInput() ?>
 
 	<?=
-	$form->field($model, 'rememberMe', [
-		'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-	])->checkbox()
+	$form->field($model, 'rememberMe')->checkbox([], false)
 	?>
 
     <div class="form-group">
